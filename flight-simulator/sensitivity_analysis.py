1045b4d5b1e256878f71f7f67beebaf8a97530b6
chasgior214 <chasgior214@gmail.com>
2024-01-23 13:22:02 -0500

    small analyses of sensitivity analysis

--- flight-simulator/sensitivity_analysis.py
+++ flight-simulator/sensitivity_analysis.py
@@ -8,12 +8,13 @@ import numpy as np
 import constants as con
 import rocket_classes as rktClass
 import flight_simulation as fs
+import helper_functions as hfunc
 from configs import Hyperion
 
 # Outline of sensitivity analysis:
 launch_rail_angles = 90 - np.linspace(5, 15, 6)
 launchpad_temps = np.linspace(25, 45, 3)
-launchpad_pressures = np.linspace(80000, 90000, 3)
+launchpad_pressures = np.linspace(80000, 90000, 3) # [86400]
 rocket_dry_masses = np.linspace(16, 19, 4)
 
 # Create a list of all the different combinations of launch conditions to be simulated with
@@ -45,21 +46,52 @@ for rocket_dry_mass in rocket_dry_masses:
         )
     )
 
+# Run the simulations
 num_sims = len(launch_conditions) * len(rockets)
 print(f"Running {num_sims} simulations")
 sim_num = 0
 apogees = []
-# max_qs = []
-# max_machs = []
+max_qs = []
+max_machs = []
 for rocket in rockets:
     for launch_condition in launch_conditions:
         sim_num += 1
         flight = fs.simulate_flight(rocket, launch_condition, 0.01)[0]
         apogee = flight['height'].iloc[-1]
         apogees.append(apogee)
+        max_qs.append(max(flight['q']))
+        # max mach number is at max speed. Use that and the temperature at that point with the function mach_number_fn in helper_functions.py
+        temp_at_max_speed = flight['temperature'].iloc[flight['speed'].idxmax()]
+        max_machs.append(hfunc.mach_number_fn(flight['speed'].max(), temp_at_max_speed))
     print(f"Simulation {sim_num} of {num_sims} complete")
 
-print(min(apogees))
-print(max(apogees))
-print(np.mean(apogees))
-print(np.std(apogees))
+# Print out the results
+apogees = np.array(apogees)*con.m_to_ft_conversion
+print('\nApogees (ft):')
+print(f'Min: {min(apogees)}')
+print(f'Max: {max(apogees)}')
+print(f'Mean: {np.mean(apogees)}')
+print(f'Std: {np.std(apogees)}')
+
+max_qs = np.array(max_qs)
+print('\nMax-qs (Pa):')
+print(f'Min: {min(max_qs)}')
+print(f'Max: {max(max_qs)}')
+print(f'Mean: {np.mean(max_qs)}')
+print(f'Std: {np.std(max_qs)}')
+
+max_machs = np.array(max_machs)
+print('\nMax Mach numbers:')
+print(f'Min: {min(max_machs)}')
+print(f'Max: {max(max_machs)}')
+print(f'Mean: {np.mean(max_machs)}')
+print(f'Std: {np.std(max_machs)}')
+
+# Plot the results
+import matplotlib.pyplot as plt
+# histogram of apogees
+plt.hist(apogees, bins=20)
+plt.title('Apogee distribution')
+plt.xlabel('Apogee (ft)')
+plt.ylabel('Frequency')
+plt.show()
