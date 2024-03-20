7a4802e75dc4032400e8548278bb4031a3810e2a
CameronBrooks11 <cambrooks3393@gmail.com>
2024-03-20 02:37:37 -0400

    added altitude estimator test

--- software-controls/dmat_main/dmat_main_old.ino
+++ software-controls/dmat_main/dmat_main_old.ino
@@ -1,12 +1,6 @@
 #include "Arduino.h"
 #include "Arduino_BHY2Host.h"
-
-#include <Wire.h>
-
-#include "altitude.h"
-
-#include "altitude_eqn.h"
-#include "DCMotorServo.h" // from https://github.com/CameronBrooks11/DCMotorServo
+//#include "DCMotorServo.h" // from https://github.com/CameronBrooks11/DCMotorServo
 
 SensorXYZ accel(SENSOR_ID_ACC);
 SensorOrientation ori(SENSOR_ID_ORI);
@@ -15,23 +9,6 @@ SensorXYZ linaccel(SENSOR_ID_LACC);
 SensorXYZ gyro(SENSOR_ID_GYRO);
 
 
-// Altitude estimator
-static AltitudeEstimator altitude = AltitudeEstimator(0.0005, // sigma Accel
-        0.0005, // sigma Gyro
-        0.018,   // sigma Baro
-        0.5, // ca
-        0.1);// accelThreshold
-
-static void imuRead(float gyro[3], float accel[3])
-{
-      gyro[0] = gyro.x()*PI/180.0/4096.0; // radians/second
-      gyro[1] = gyro.y()*PI/180.0/4096.0;
-      gyro[2] = gyro.z()*PI/180.0/4096.0;
-      // and acceleration values
-      accel[0] = linaccel.x()/4096.0; // g-unit
-      accel[1] = linaccel.y()/4096.0;
-      accel[2] = linaccel.z()/4096.0;
-}
 
 void setup() {
   // debug port
@@ -47,40 +24,18 @@ void setup() {
   baro.begin();
   linaccel.begin();
   gyro.begin();
+
+
+
+
 }
 
 
 void loop()
 {
+  static auto printTime = millis();
   BHY2Host.update();
 
-  currentTime = millis();
-  if ((currentTime - pastTime) > 50)
-  {
-    // get all necessary data
-    float pressure = baro.value();
-    baro.update(pressure);
-    // Assuming pressure is in Pa, converting to millibars by dividing by 100    
-
-    float baroHeight = AltitudeEquation::pressureToAltitudeMeters(pressure / 100); 
-    uint32_t timestamp = micros();
-    float accelData[3];
-    float gyroData[3];
-    imuRead(gyroData, accelData);
-    altitude.estimate(accelData, gyroData, baroHeight, timestamp);
-    Serial.print(baroHeight);
-    Serial.print(",");
-    Serial.print(altitude.getAltitude());
-    Serial.print(",");
-    Serial.print(altitude.getVerticalVelocity());
-    Serial.print(",");
-    Serial.println(altitude.getVerticalAcceleration());
-    pastTime = currentTime;
-  }
-
-
-/*
-static auto printTime = millis();
   if (millis() - printTime >= 500) {
     printTime = millis();
     Serial.println(String("Acceleration values: ") + accel.toString());
@@ -89,8 +44,6 @@ static auto printTime = millis();
     Serial.println(String("Linear Acceleration: ") + linaccel.toString());
     printVelocity();
   }
-*/
-
 }
 
 
@@ -117,5 +70,4 @@ void printVelocity()
   Serial.print(" rad/s, Z: ");
   Serial.print(gyro_data.z, 6);
   Serial.println(" rad/s");
-
 }
